824b49050ad91ff487e7b79cecbe6ab611ef8981
Niels-Erik Nandal <nielseriknandal@gmail.com>
2020-10-13 03:58:31 +0000

    added 'create' action to save 'contact us' to db

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,19 @@
 class ContactsController < ApplicationController
     def new
+        @contact = Contact.new #now everytime someone goes to the form page, rails will create a new blank object (consisting of the items you described in the db) and stores it in this "@contact" instance variable. you can refer to this @contact in the View html file
     end
+    
+    def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            redirect_to new_contact_path, notice: "Message sent."
+        else
+            redirect_to new_contact_path, note: "Error occurred, message not sent."
+        end
+    end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
