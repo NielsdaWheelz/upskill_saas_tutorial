f79da6781a1c29174ea2c50692d757c2cd7792ec
Niels-Erik Nandal <8474319+NielsdaWheelz@users.noreply.github.com>
2020-10-14 16:58:40 +0000

    Added contact form view, route, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
     root to: 'pages#home' #root (the home, index) needs to point to the pages controller (pages_controller) and then to the 'home' method/function
     get 'about', to: 'pages#about'
+    resources :contacts
 end
