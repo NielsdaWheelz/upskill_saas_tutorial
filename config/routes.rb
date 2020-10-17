5275bbe591ca57bb91975631a1536fb77be9295e
Niels-Erik Nandal <8474319+NielsdaWheelz@users.noreply.github.com>
2020-10-17 18:43:08 +0000

    added custom contact form path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
     root to: 'pages#home' #root (the home, index) needs to point to the pages controller (pages_controller) and then to the 'home' method/function
     get 'about', to: 'pages#about'
     resources :contacts
+    get 'contact-us', to: 'contacts#new'
 end
